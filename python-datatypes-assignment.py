21478280c4244432b5f81d3557558117a118f5d3
Ryan Gael <ryang601@gmail.com>
2024-12-21 18:07:04 +0000

    data-types-assignment

---
+++ python-datatypes-assignment.py
@@ -0,0 +1,10 @@
+python_datatypes = [1,1.5,"Hello",True]
+my_dictionary = {
+    "name" : "Ryan",
+    "age" : 28,
+    "Location" : "London"
+}
+print(my_dictionary["name"])
+print(my_dictionary["age"])
+print(my_dictionary["Location"])
+
