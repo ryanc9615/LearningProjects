3113fe2f294ce48795df83cc5811b67e955f92d5
Ryan Gael <ryang601@gmail.com>
2025-01-06 21:14:31 +0000

    conditional statements

---
+++ conditional-statements.py
@@ -0,0 +1,12 @@
+student_score = int(input("Please enter the students score between 0 and 100: "))
+
+if student_score >= 90: 
+    print("Grade A")
+elif student_score >= 80:
+    print("Grade B")
+elif student_score >= 70:
+    print("Grade C")
+elif student_score >= 60:
+    print("Grade D")
+else:
+    print("Grade F")
